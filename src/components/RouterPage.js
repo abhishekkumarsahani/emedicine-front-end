c782e0a2b384d25908fdded25622f0c067c1c638
abhishekkumarsahani <126187842+abhishekkumarsahani@users.noreply.github.com>
2024-01-27 22:15:24 +0545

    UpdateRouterpage

--- src/components/RouterPage.js
+++ src/components/RouterPage.js
@@ -7,6 +7,10 @@ import Orders from './users/Orders';
 import Profile from './users/Profile';
 import Cart from './users/Cart';
 import AdminDashboard from './admin/AdminDashboard';
+import AdminOrders from './admin/AdminOrders';
+import CustomerList from './admin/CustomerList';
+import Medicine from './admin/Medicine';
+import MedicineDisplay from './users/MedicineDisplay';
 export default function RouterPage(){
     return(
         <Router>
@@ -17,10 +21,12 @@ export default function RouterPage(){
             <Route path='/' element={<Orders/>} />
             <Route path='/' element={<Profile/>} />
             <Route path='/' element={<Cart/>} />
+            <Route path='/' element={<MedicineDisplay/>} />
 
             <Route path='/' element={<AdminDashboard/>} />
-            <Route path='/' element={<Admin/>} />
-            <Route path='/' element={</>} />
+            <Route path='/' element={<AdminOrders/>} />
+            <Route path='/' element={<CustomerList/>} />
+            <Route path='/' element={<Medicine/>} />
             </Routes>
         </Router>
     )
