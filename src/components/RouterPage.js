3b55fd58fae50faad5918e4f38f0729606cd62b7
abhishekkumarsahani <126187842+abhishekkumarsahani@users.noreply.github.com>
2024-01-28 16:14:10 +0545

    EachPage is Updated
    The page is modified for the ui design

--- src/components/RouterPage.js
+++ src/components/RouterPage.js
@@ -1,32 +1,40 @@
-import React from 'react';
+import React from  'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Login from './Login';
-import Registration from './Registration';
-import Dashboard from './users/Dashboard';
-import Orders from './users/Orders';
-import Profile from './users/Profile';
-import Cart from './users/Cart';
 import AdminDashboard from './admin/AdminDashboard';
 import AdminOrders from './admin/AdminOrders';
 import CustomerList from './admin/CustomerList';
 import Medicine from './admin/Medicine';
+import Login from './Login';
 import MedicineDisplay from './users/MedicineDisplay';
+import Registration from './Registration';
+import Cart from './users/Cart';
+import Dashboard from './users/Dashboard';
+import Orders from './users/Orders';
+import Profile from './users/Profile';
+import Receipt from './users/Receipt';
+import SideMenu from './users/SideMenu';
+
 export default function RouterPage(){
+    
     return(
         <Router>
             <Routes>
-            <Route path='/' element={<Login/>} />
-            <Route path='/' element={<Registration/>} />
-            <Route path='/' element={<Dashboard/>} />
-            <Route path='/' element={<Orders/>} />
-            <Route path='/' element={<Profile/>} />
-            <Route path='/' element={<Cart/>} />
-            <Route path='/' element={<MedicineDisplay/>} />
+                <Route exact path='/' element={ <Login /> } />
+                <Route path='/registration' element={ <Registration /> } />
+                <Route path='/dashboard' element={ <Dashboard /> } />                
+                <Route path='/myorders' element={ <Orders /> } />
+                <Route path='/profile' element={ <Profile /> } />
+                <Route path='/cart' element={ <Cart /> } />
 
-            <Route path='/' element={<AdminDashboard/>} />
-            <Route path='/' element={<AdminOrders/>} />
-            <Route path='/' element={<CustomerList/>} />
-            <Route path='/' element={<Medicine/>} />
+                <Route path='/admindashboard' element={ <AdminDashboard /> } />
+                <Route path='/adminorders' element={ <AdminOrders /> } />
+                <Route path='/customers' element={ <CustomerList /> } />
+                <Route path='/medicine' element={ <Medicine /> } />
+
+                <Route path='/products' element={ <MedicineDisplay /> } />
+
+                <Route path='/receipt/:id' element={ <Receipt /> } />
+                <Route path='/sidemenu' element={ <SideMenu /> } />
             </Routes>
         </Router>
     )
---
+++ src/components/constants.js
@@ -0,0 +1,1 @@
+export const baseUrl = 'https://localhost:44341/swagger/index.html';
